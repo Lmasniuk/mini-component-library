e5248dd2946dcf060a08bb40f732f004e97075b6
Logan Masniuk <logan.masniuk@gmail.com>
2025-07-17 17:06:12 -0700

    Style progress bar to match figma mockups

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,24 +1,65 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-
-  return <div>
-    <label>{value}%</label>
-    <StyledProgressBar max={100} value={value}></StyledProgressBar>
-
-
-  </div>
-  
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--progressBarBorderRadius": "4px",
+    "--progressBoxBorderRadius": "4px",
+  },
+  medium: {
+    "--height": "12px",
+    "--progressBarBorderRadius": "4px",
+    "--progressBoxBorderRadius": "4px",
+  },
+  large: {
+    "--height": "24px",
+    "--progressBarBorderRadius": "4px",
+    "--progressBoxBorderRadius": "8px",
+    "--padding": "4px",
+  },
 };
 
+const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+  return (
+    <div>
+      <ProgressLabel>{value}%</ProgressLabel>
+      <StyledProgressBar
+        max={100}
+        value={value}
+        style={styles}
+      ></StyledProgressBar>
+    </div>
+  );
+};
+
+const ProgressLabel = styled.h1`
+  margin-left: 5rem;
+  margin-bottom: 1rem;
+`;
 
 const StyledProgressBar = styled.progress`
+  appearance: none;
+  width: 70%;
+  height: var(--height);
 
+  &::-webkit-progress-bar {
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    border-radius: var(--progressBoxBorderRadius);
+    background-color: ${COLORS.transparentGray35};
+    padding: var(--padding);
+  }
+
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: var(--progressBarBorderRadius) 0 0
+      var(--progressBarBorderRadius);
+  }
 `;
 
 export default ProgressBar;
