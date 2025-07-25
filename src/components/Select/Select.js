37f851a8b0bab5f63287e476392c57989413b141
Logan Masniuk <logan.masniuk@gmail.com>
2025-07-25 13:36:21 -0700

    Start to build custom select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const NativeSelect = styled.select`
+  opacity: 0;
+  position: absolute;
+  top: 2rem;
+`;
+
+const CustomSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 1rem;
+
+  position: absolute;
+  top: 2rem;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <CustomSelect>
+        {value}
+        <Icon id="chevron-down" />
+      </CustomSelect>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+    </>
   );
 };
 
